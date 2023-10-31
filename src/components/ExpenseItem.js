d94007476f7e0cfcd32fd48770b548457bf83896
hansol na <pinecone171@gmail.com>
2023-10-31 13:14:38 +0900

    3. 컴포넌트 분할

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = ({ date, title, price: propsPrice }) => {
   // console.log(props);
@@ -28,7 +29,7 @@ const ExpenseItem = ({ date, title, price: propsPrice }) => {
 
   return (
     <div className='expense-item'>
-      <div>{makeFormattedDate()}</div>
+      <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>{formattedPrice}원</div>
