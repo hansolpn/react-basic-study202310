f3951e7beb668b1783e1dc89af6fd80bcae668bc
hansol na <pinecone171@gmail.com>
2023-11-01 17:38:56 +0900

    231101 react usestate 동적리스트

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -8,8 +8,8 @@ const ExpenseDate = ({ date }) => {
 
   return (
     <div className='expense-date'>
-      <div className='expense-date__month'>{year}</div>
-      <div className='expense-date__year'>{month}</div>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
       <div className='expense-date__day'>{day}</div>
     </div>
   );
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = ({ onSaveExpense }) => {
+  // const [title, setTitle] = useState('');
+  // const [price, setPrice] = useState('');
+  // const [date, setDate] = useState('');
+
+  const [userInput, setUserInput] = useState({
+    title: '',
+    price: '',
+    date: '',
+  });
+
+  const titleChangeHandler = (e) => {
+    // setTitle(e.target.value);
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        title: e.target.value,
+      };
+    });
+  };
+
+  const priceChangeHandler = (e) => {
+    // setPrice(e.target.value);
+    setUserInput({
+      ...userInput,
+      price: e.target.value,
+    });
+  };
+
+  const dateChangeHandler = (e) => {
+    // setDate(e.target.value);
+    setUserInput({
+      ...userInput,
+      date: e.target.value,
+    });
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault(); // submit 차단
+    console.log('submit 버튼을 누름!');
+
+    onSaveExpense(userInput);
+
+    // 입력창 리셋
+    setUserInput({
+      title: '',
+      price: '',
+      date: '',
+    });
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input
+            type='text'
+            onChange={titleChangeHandler}
+            value={userInput.title}
+          />
+        </div>
+        <div className='new-expense__control'>
+          <label>Price</label>
+          <input
+            type='number'
+            min='100'
+            step='100'
+            onChange={priceChangeHandler}
+            value={userInput.price}
+          />
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2025-12-31'
+            onChange={dateChangeHandler}
+            value={userInput.date}
+          />
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
