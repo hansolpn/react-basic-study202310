51cc9a8d070069d9d4c1ca32c2dcc6e9cca8f617
hansol na <142369934+hansolpn@users.noreply.github.com>
2023-11-14 12:14:56 +0900

    231114 종합예제 템플릿

--- README.md
+++ README.md
@@ -22,3 +22,13 @@ $ npm install
 
 이유: node_modules 폴더는 github에 포함되어 있지 않기 때문에
 다른 환경에서는 라이브러리 다운로드 및 설치가 반드시 필요!
+
+# 추가 라이브러리 설치
+
+1. styled-components: 동적 css 처리를 도와주는 라이브러리.
+
+- `$ npm install styled-components`
+
+2. css 확장 라이브러리 sass
+
+- `$npm install sass`
--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React, { useContext } from 'react';
-import MainHeader from './components/SideEffect/MainHeader/MainHeader';
-import Home from './components/SideEffect/Home/Home';
-import Login from './components/SideEffect/Login/Login';
-
-// 컨텍스트 불러오기
-import AuthContext from './store/auth-context';
+import React from 'react';
+import Header from './components/Food/Layout/Header';
+import Meals from './components/Food/Meals/Meals';
 
 const App = () => {
-  // 화면이 리렌더링 될 때 localStorage를 확인해서
-  // 현재 login-flag가 존재하는지 검사
-  console.log('app.js render');
-  const { isLoggedIn } = useContext(AuthContext);
-
   return (
     <>
-      <MainHeader />
-      <main>
-        {isLoggedIn && <Home />}
-        {!isLoggedIn && <Login />}
-      </main>
+      <Header />
+      <div id='main'>
+        <Meals />
+      </div>
     </>
   );
 };
